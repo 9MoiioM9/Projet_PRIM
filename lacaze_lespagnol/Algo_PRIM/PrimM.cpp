0295477fe5350c77ed42250e8c99714d02234524
ylacaze <72870976+ylacaze@users.noreply.github.com>
2024-01-27 17:07:00 +0100

    Structuration de PrimM

--- lacaze_lespagnol/Algo_PRIM/PrimM.cpp
+++ lacaze_lespagnol/Algo_PRIM/PrimM.cpp
@@ -3,3 +3,49 @@
 //
 
 #include "PrimM.h"
+
+PrimM::Matrice::Matrice(int nbSommet) : nbSommet(nbSommet){
+    this->matrice = new int[nbSommet*nbSommet];
+    for (int i = 0; i < nbSommet*nbSommet; ++i) {
+        matrice[i]=0;
+    }
+}
+
+int PrimM::Matrice::get(int sommet1, int sommet2) {
+    return this->matrice[(sommet1-1)+ nbSommet*(sommet2-1)];
+}
+
+int PrimM::Matrice::set(int sommet1, int sommet2, int value) {
+    matrice[(sommet1-1)+ nbSommet*(sommet2-1)] = value;
+    matrice[(sommet2-1)+ nbSommet*(sommet1-1)] = value;
+}
+
+
+PrimM::PrimM(std::string input, int sommet) : sommet(sommet), nbSommet(0), afficheEcran(true), output(
+        nullptr){
+    this->input = new std::ifstream();
+    this->input->open(input);
+    this->enregistrerMatrice();
+}
+
+
+
+PrimM::PrimM(std::string input, int sommet, std::string output) : sommet(sommet), nbSommet(0), afficheEcran(false){
+    this->input = new std::ifstream(input);
+    this->input->open(input);
+    this->output = new std::ofstream(output);
+    this->output->open(output);
+    this->enregistrerMatrice();
+}
+
+void PrimM::enregistrerMatrice() {
+    char* nombre;
+    input->getline(nombre,INT_MAX,' ');
+    this->nbSommet = std::stoi(nombre);
+    matriceAdjacence = new Matrice(nbSommet);
+
+    //TODO remplir la matrice
+}
+
+
+
--- lacaze_lespagnol/Algo_PRIM/PrimM.h
+++ lacaze_lespagnol/Algo_PRIM/PrimM.h
@@ -5,9 +5,34 @@
 #ifndef LACAZE_LESPAGNOL_PRIMM_H
 #define LACAZE_LESPAGNOL_PRIMM_H
 
+#include <string>
+#include <fstream>
 
 class PrimM {
+private:
+    class Matrice{
+    private:
+        int nbSommet;
+        int *matrice;
+    public:
+        Matrice(int nbSommet);
+        int get(int sommet1,int sommet2);
+        int set(int sommet1,int sommet2, int value);
+    };
 
+
+    Matrice* matriceAdjacence;
+    int sommet;
+    bool afficheEcran;
+    std::ofstream* output;
+    std::ifstream* input;
+    int nbSommet;
+
+    void enregistrerMatrice();
+
+public:
+    PrimM(std::string input, int sommet);
+    PrimM(std::string input, int sommet,std::string output);
 };
 
 
